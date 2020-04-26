490a86d1c9c3d1f7de69ecd24d7c99b39dc375e6
Diego Sarmiento <44205748+diegolsarmiento@users.noreply.github.com>
2020-04-26 16:02:39 -0400

    Fixed typo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 function fib(num) {
- // Inneficient but does the job
 	if (num < 2) return num;
-    
+    // Inefficient but does the job
     return fib(num-1) + fib(num-2);
 }
  console.log(fib(8));
